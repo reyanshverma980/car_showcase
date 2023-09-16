3de8e56ef6a94ef2c07c3fb26eb884a7567cd973
Reyansh <ronitverma0202@gmail.com>
2023-09-16 12:51:39 +0530

    fixed heo button click

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -4,7 +4,13 @@ import Image from "next/image";
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
-  const handleScroll = () => {};
+  const handleScroll = () => {
+    const nextSection = document.getElementById("discover");
+
+    if (nextSection) {
+      nextSection.scrollIntoView({ behavior: "smooth" });
+    }
+  };
 
   return (
     <div className="hero">
